c71889a986e4cf88433834beb63aa57683dd00d5
moviefan322 <philman202@yahoo.com>
2023-05-03 14:36:36 -0400

    adds front end functionality, retrieving posts from api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const path = require("path");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
@@ -8,9 +10,20 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Enable CORS
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to the Idea App!" });
